5cc7dbb0ce4156b33af3c313d32b675694a43ca5
Brooks Townsend <brooksmtownsend@gmail.com>
2025-04-02 15:06:48 +0100

    feat: support showing failed error, reload transactions
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- wasmpay-ui/src/features/dashboard/components/dashboard.tsx
+++ wasmpay-ui/src/features/dashboard/components/dashboard.tsx
@@ -3,19 +3,22 @@ import {AccountBalance} from './account-balance';
 import {Transactions} from '@/features/transactions/components/transactions';
 import {Chat} from '@/features/chat/components/chat';
 import {TransactionModal} from '@/features/transactions/components/transaction-modal';
+import {useTransactions} from '@repo/common/hooks/useTransactions';
 
 function Dashboard(): React.ReactElement {
+  const {transactions, balance, isLoading, reloadTransactions} = useTransactions();
+
   return (
     <div className="grid grid-cols-12 gap-3">
       <div className="col-span-12 md:col-span-6">
         <Chat />
       </div>
       <div className="col-span-12 md:col-span-6">
-        <AccountBalance />
+        <AccountBalance transactions={transactions} balance={balance} isLoading={isLoading} />
       </div>
       <div className="col-span-12">
-        <Transactions />
-        <TransactionModal />
+        <Transactions transactions={transactions} isLoading={isLoading} />
+        <TransactionModal reloadTransactions={reloadTransactions} />
       </div>
     </div>
   );
